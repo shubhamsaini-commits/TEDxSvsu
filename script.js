b029b313d328473bc9b345fc547378da103cfb5e
shubhamsaini-commits <shubham.saini.dev.cse@gmail.com>
2025-11-07 19:50:45 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ VANTA.NET({
   minWidth: 200.00,
   scale: 1.00,
   scaleMobile: 1.00,
-  color: 0xf50039,
+  color: 0xff0000,
   backgroundColor: 0x0,
   maxDistance: 22.00,
   spacing: 16.00
@@ -25,3 +25,25 @@ ham.addEventListener("click" , ()=>{
     ham.classList.toggle("activeHam")
     menu.classList.toggle("activeMenu")
 })
+
+const eventDate = new Date("November 26, 2025 10:00:00").getTime();
+
+
+function updateTimer() {
+  const now = new Date().getTime();            
+  const diff = eventDate - now;                 
+  // If the event has already started
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+  // Display it
+  document.querySelector(".day .value").textContent = days;
+  document.querySelector(".hour .value").textContent = hours;
+  document.querySelector(".min .value").textContent = minutes;
+  document.querySelector(".sec .value").textContent = seconds;
+  }
+
+  const timerInterval = setInterval(updateTimer, 1000);
+updateTimer();
